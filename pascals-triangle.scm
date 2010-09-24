a736e0ad2f35c63e33912ca2963b2ab85a0ad62b
Andrew Buntine <bunts@hhd.com.au>
2010-09-24 16:01:51 +1000

    Added in functionality for generating the elements of Pascal's triangle to n width/rows

--- pascals-triangle.scm
+++ pascals-triangle.scm
@@ -16,8 +16,41 @@
 (module pascals-triangle mzscheme
   (require (lib "graphics.ss" "graphics"))
 
+  (define *ROWS* 64)
+  (define *CELL* '(4 4))
+  (define *WIDTH* (* (car *CELL*) *ROWS*))
+  (define *HEIGHT* (* (cadr *CELL*) *ROWS*))
+
+  ; Generates the elements of Pascal's triangle to
+  ; the given width (rows). The triangle is represented
+  ; as a list of expanding sublists - one for each row.
+  (define (pascals-triangle width)  
+    (reverse (pascal-helper '((1)) width)))  
+
+  (define (pascal-helper rows width)  
+    (if (= (length rows) width)  
+      rows  
+      (pascal-helper (cons (next-row (car rows))  
+                           rows)  
+                     width)))  
+
+  ; Generates the next row, given the current one.
+  (define (next-row previous)  
+    (let ((body (row-body '() previous)))  
+      (if (null? body)  
+        '(1 1)  
+        (cons 1 (append body '(1))))))  
+
+  ; Returns a list of the next rows body (leaving off the 1's).
+  (define (row-body body previous)  
+    (if (null? (cdr previous))  
+      body  
+      (row-body (cons (+ (car previous) (cadr previous))  
+                      body)  
+                (cdr previous))))  
+
   ; Initialising function. This is the one you should invoke!
   (define (draw-pascals-triangle)
-    '())
+    (pascals-triangle *ROWS*))
 
   (provide draw-pascals-triangle))
